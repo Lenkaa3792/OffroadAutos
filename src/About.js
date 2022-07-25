f111a2d8120e40dbffcba5438883c9d4505490d8
Lenkaa3792 <allanlenkaa@gmail.com>
2022-07-25 11:59:56 +0300

    adding components to the files

---
+++ src/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+export default function About ()
+{
+    return (
+        <div id="about">
+            <h2>about offroad Site</h2>
+            
+        </div>
+    )
+    
+}
---
+++ src/Nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export function Nav ()
+{
+    return (
+      <nav>
+        <ul className="navlink">
+          <li>About</li>
+          <li>Tracks</li>
+          <li>Vehicles</li>
+        </ul>
+      </nav>
+    );
+}
