e669b7856041f851cab7b30042abc6c91107a204
Lenkaa3792 <allanlenkaa@gmail.com>
2022-07-25 15:03:09 +0300

    updates

--- src/About.js
+++ src/About.js
@@ -4,10 +4,22 @@ import React from "react";
 export default function About ()
 {
     return (
-        <div id="about">
-            <h2>about offroad Site</h2>
-            
-        </div>
-    )
+      <div id="about">
+        <strong>
+          <h1>What is Offroading</h1>
+        </strong>
+        <p>
+          Off-roading is the activity of driving or riding in a vehicle on
+          unsurfaced roads or tracks, surfaces such as sand, gravel, riverbeds,
+          mud, snow, rocks, and other natural terrain.
+        </p>
+        <p>
+          All-terrain vehicles (ATVs), off-highway motorcycles or dirt bikes,
+          Utility Terrain Vehicles (UTVs or side-by-side vehicles), snowmobiles,
+          and four-wheel-drive vehicles (such as jeeps and trucks) can all be
+          defined as OHVs.
+        </p>
+      </div>
+    );
     
 }
--- src/Vehicles.js
+++ src/Vehicles.js
@@ -1,5 +1,46 @@
 import React from "react";
+import Image1 from './Image/jeep.jpg'
+import Image2 from "./Image/ford.jpg";
+import Image3 from "./Image/rover.jpg";
 export default function Vehicles ()
 {
-   
-} 
+    return (
+      <div>
+        <h2>Best Off-Road Vehicles Currently Available</h2>
+
+        <section className="jeep">
+          <h3>Jeep Wrangler Rubicon</h3>
+          <img src={Image1} />
+          <p>
+            Jeep Wrangler is the king of the castle. If you want the best of the
+            best, look toward the Rubicon trim.
+          </p>
+        </section>
+        <section className="ford">
+          <h3>Ford F-150 Raptor</h3>
+          <img src={Image2} />
+          <p>
+            While the majority of off-roading vehicles on this list are all
+            about hitting trails, the F-150 Raptor is more interested in
+            high-speed off-roading. That’s why it comes with a 450-horsepower
+            twin-turbocharged V6 engine and Fox Racing dampers that offer 13
+            inches of travel in the front and 13.9 inches at the rear.
+          </p>
+        </section>
+        <section className="rover">
+          <h3>Land Rover Defender</h3>
+          <img src={Image3} />
+          <p>
+            The Land Rover Defender nameplate for the 2020 model year has been
+            resurrected, bringing back one of the most capable off-roaders ever
+            envisioned in a modern luxury vehicle. While the Defender might not
+            have a body-on-frame setup or solid axles, the SUV can still go
+            anywhere and do anything.{" "}
+          </p>
+        </section>
+      </div>
+    );
+       
+       
+
+    }
