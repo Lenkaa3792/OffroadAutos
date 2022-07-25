aaa66efedb4c179eae44d2088da6803e81ca7743
Lenkaa3792 <allanlenkaa@gmail.com>
2022-07-25 12:29:49 +0300

    paths to the different pages

--- src/App.js
+++ src/App.js
@@ -4,15 +4,23 @@ import { Nav } from './Nav';
 import Vehicles from './Vehicles';
 import About from './About';
 import Tracks from './Tracks';
+import {BrowserRouter,Route, Switch} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <About />
-      <Tracks />
+      <BrowserRouter>
+        <Nav />
+        
+          <Route path="/about" exact component={About} />
+          <Route path="/tracks" exact component={Tracks} />
+          <Route path="/vehicles" exact component={Vehicles} />
+        
+      </BrowserRouter>
     </div>
   );
 }
+
+
 
 export default App;
